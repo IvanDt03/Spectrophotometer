985ca14bba3121ab24d0f32d6ef6191bf9ea7054
Ivan Dmitriev <vanya2003569@yandex.ru>
2025-05-26 16:08:37 +0300

    Добавить RelayCommand и Notifier

---
+++ Spectrophotometer/Commands/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Spectrophotometer.Commands;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action<object?>? _execute;
+    private readonly Func<object?, bool>? _canExecute;
+
+    public RelayCommand(Action<object?> execute, Func<object?, bool> canExecute)
+    {
+        if (execute is null)
+            throw new ArgumentNullException(nameof(execute));
+
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public void Execute(object? parameter) => _execute?.Invoke(parameter);
+    public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CommandManager.InvalidateRequerySuggested();
+    }
+}
---
+++ Spectrophotometer/ViewModels/Notifier.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Spectrophotometer.ViewModels;
+
+public class Notifier : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
+    {
+        if (PropertyChanged != null)
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual bool SetValue<T>(ref T field, T value, [CallerMemberName]string propertyName = "")
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+            return false;
+
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
